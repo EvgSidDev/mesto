c64e7f50390c1588c1f8b5515ed67385ba68f51f
Evgeny <SidorovEvg.Sid@yandex.ru>
2023-06-10 14:39:49 +0300

    features: v1.0

---
+++ src/app/script.js
@@ -0,0 +1,47 @@
+const popup = document.querySelector(".popup");
+
+const editButtonProfile = document.querySelector(".profile__edit-button");
+const closeButtonProfile = document.querySelector(".popup__close-button");
+const submitButtonProfile = document.querySelector(".popup__submit");
+const likeButton = document.querySelectorAll(".element__like")
+
+const profileName = document.querySelector(".profile__name");
+const profileStatus = document.querySelector(".profile__status");
+const popupTextInputs = document.querySelectorAll(".popup__text-input");
+const popupName = popupTextInputs[0];
+const popupStatus = popupTextInputs[1];
+
+
+togglePopup();
+
+function openEditPopupProfile(e) {
+  popupName.value = profileName.innerText;
+  popupStatus.value = profileStatus.innerText;
+  togglePopup();
+}
+
+function togglePopup(e) {
+  popup.classList.toggle("popup_hidden");
+}
+
+function saveChangesProfileEvent(e) {
+  saveChangesProfile();
+}
+
+function saveChangesProfileKeyDown(e) {
+  if (e.keyCode === 13) {
+    saveChangesProfile();
+  }
+}
+
+function saveChangesProfile() {
+  profileName.innerText = popupName.value;
+  profileStatus.innerText = popupStatus.value;
+  togglePopup();
+}
+
+editButtonProfile.addEventListener('click', openEditPopupProfile);
+closeButtonProfile.addEventListener('click', togglePopup);
+submitButtonProfile.addEventListener('click', saveChangesProfileEvent);
+popup.addEventListener('keydown', saveChangesProfileKeyDown);
+
