e6fac83753d986d1df3a110d32800d03186d8496
Evgeny <SidorovEvg.Sid@yandex.ru>
2023-06-14 08:35:51 +0300

    fix: bugs

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const popup = document.querySelector(".popup");
+
+const editButtonProfile = document.querySelector(".profile__edit-button");
+const closeButtonProfile = document.querySelector(".popup__close-button");
+
+const profileName = document.querySelector(".profile__name");
+const profileStatus = document.querySelector(".profile__status");
+const popupName = document.querySelector("#input-name");
+const popupStatus = document.querySelector("#input-status");
+
+function openEditPopupProfile() {
+  popupName.value = profileName.textContent;
+  popupStatus.value = profileStatus.textContent;
+  togglePopup();
+}
+
+function togglePopup() {
+  popup.classList.toggle("popup_opened");
+}
+
+function saveChangesProfileEvent(e) {
+  saveChangesProfile();
+  e.preventDefault();
+}
+
+function saveChangesProfile() {
+  profileName.textContent = popupName.value;
+  profileStatus.textContent = popupStatus.value;
+  togglePopup();
+}
+
+editButtonProfile.addEventListener('click', openEditPopupProfile);
+closeButtonProfile.addEventListener('click', togglePopup);
+popup.addEventListener('submit', saveChangesProfileEvent);
+
