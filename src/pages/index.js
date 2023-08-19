d1d851a05362d216e9fb24b44db4c3e2a1753c0f
Evgeny <SidorovEvg.Sid@yandex.ru>
2023-08-19 04:32:06 +0300

    fix bugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-// import "./index.css";
+import "./index.css";
 import Card from "../components/Card.js";
 import FormValidation from "../components/FormValidation.js";
 import PopupWithForm from "../components/PopupWithForm.js";
